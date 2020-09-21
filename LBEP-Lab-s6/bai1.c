183432eee316cf9d5db587685ee86bb974c34ccd
thinhnw <thinhnv0412@gmail.com>
2020-09-21 10:43:19 +0700

    Initial commit

---
+++ LBEP-Lab-s6/bai1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+const int N = 1e6;
+
+void main() {
+    int n;
+    int a[N];
+    printf("n = ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i) {
+        printf("a[%d] = ", i);
+        scanf("%d", &a[i]);
+    }
+    
+    double avg = 0; // ban dau tinh tong, cuoi cung lay trung binh
+    int cnt = 0; // dem so so le
+    for (int i = 0; i < n; ++i) {
+        if (a[i] % 2) {
+            avg += a[i];
+            cnt++;
+        }
+    }
+    avg = avg/cnt;
+    printf("Average = %lf\n", avg);
+}
