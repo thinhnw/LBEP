d325e1eb35d6139427f138e6733748914f444f9d
thinhnw <thinhnv0412@gmail.com>
2020-09-21 10:47:12 +0700

    Add bai 2

---
+++ LBEP-Lab-s6/bai2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+const int N = 1e6;
+
+void main() {
+    int n;
+    int a[N];
+    printf("n = ");
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i) {
+        printf("a[%d] = ", i);
+        scanf("%d", &a[i]);
+    }
+    
+    double avg = 0; // ban dau tinh tong, cuoi cung lay trung binh
+    int cnt = 0; // dem so so le
+    for (int i = 0; i < n; i += 2) {
+        if (a[i] % 2) {
+            avg += a[i];
+            cnt++;
+        }
+    }
+    if (cnt)
+        avg = avg/cnt;
+    printf("Average = %lf\n", avg);
+}
