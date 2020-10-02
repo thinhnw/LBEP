c2ad5199417d7aff5bfd234aa3916ead5626c8b9
thinhnw <thinhnv0412@gmail.com>
2020-10-02 10:22:07 +0700

    Change solution according to the statement

--- PracticalExam/exercise1.c
+++ PracticalExam/exercise1.c
@@ -59,11 +59,16 @@ void sol1() {
 	long long f[101];
 	f[0] = 0;
 	f[1] = 1;
+	int n = 0;
 	for (int i = 2; i <= 100; ++i) {
 		f[i] = f[i-1] + f[i-2];
+		if (f[i] > 100) {
+			n = i;
+			break;
+		}
 	}
 
-	for (int i = 1; i <= 100; ++i) {
+	for (int i = 1; i < n; ++i) {
 		printf("%lld ", f[i]);
 	}
 	printf("\n");
@@ -96,7 +101,7 @@ void sol2() {
 
 void main() {
 
-	// sol1();
+	sol1();
 	// test();
-	sol2();
+	// sol2();
 }
